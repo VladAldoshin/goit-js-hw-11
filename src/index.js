8444b4cc760bf56aa07749b5839b619e21aa2fce
VladAldoshin <aldoshinvladislav87@gmail.com>
2023-05-25 18:15:50 +0300

    update createMarcup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { Notify } from 'notiflix';
-// import {createMarcup} from './js/createMarcup';
+import {createMarcup} from './js/createMarcup';
 const axios = require('axios').default;
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
@@ -116,26 +116,26 @@ async function fetchImages(searchQueryResult) {
   return results;
   };
 
-  async function createMarcup(results) {
-    const { hits } = results;
-    markupData.markup = hits.map((hit) =>
-            `<a href="${hit.largeImageURL}"><div class="photo-card">
-            <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy"
-              class="img-item" />
-            <div class="info">
-        <p class="info-item">
-          <b>Likes:</b>${hit.likes}
-        </p>
-        <p class="info-item">
-          <b>Views:</b>${hit.views}
-        </p>
-        <p class="info-item">
-          <b>Comments:</b>${hit.comments}
-        </p>
-        <p class="info-item">
-          <b>Downloads:</b>${hit.downloads}
-        </p>
-      </div>
-    </div></a>`).join("");
-        return markupData.markup;
-    };
+  // async function createMarcup(results) {
+  //   const { hits } = results;
+  //   markupData.markup = hits.map((hit) =>
+  //           `<a href="${hit.largeImageURL}"><div class="photo-card">
+  //           <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy"
+  //             class="img-item" />
+  //           <div class="info">
+  //       <p class="info-item">
+  //         <b>Likes:</b>${hit.likes}
+  //       </p>
+  //       <p class="info-item">
+  //         <b>Views:</b>${hit.views}
+  //       </p>
+  //       <p class="info-item">
+  //         <b>Comments:</b>${hit.comments}
+  //       </p>
+  //       <p class="info-item">
+  //         <b>Downloads:</b>${hit.downloads}
+  //       </p>
+  //     </div>
+  //   </div></a>`).join("");
+  //       return markupData.markup;
+  //   };
